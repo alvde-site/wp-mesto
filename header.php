9dc0df131a8e09e87451622eabaa02c8b8da3dc5
Aleksandr Demidenko <sashatro@mail.ru>
2023-01-12 19:31:06 +0300

    feat: Add theme block from wordpress for Mesto

---
+++ header.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The header.
+ *
+ * This is the template that displays all of the <head> section and everything up until main.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?> <?php twentytwentyone_the_html_classes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<script defer="defer" src="<?php echo bloginfo('template_url'); ?>/main.js"></script>
+	<?php wp_head(); ?>
+</head>
+
+<body class="root">
+    <div class="page">
+      <header class="header"><div class="header__logo"></div></header>
+      <main class="content">
