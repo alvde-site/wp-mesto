756c216c6effbb05b319acda3ac04d548348e9dc
Aleksandr Demidenko <sashatro@mail.ru>
2023-02-17 10:48:07 +0300

    Refactor: Remove unnecessary files

--- header.php
+++ header.php
@@ -1,27 +1,18 @@
-<?php
-/**
- * The header.
- *
- * This is the template that displays all of the <head> section and everything up until main.
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package WordPress
- * @subpackage Twenty_Twenty_One
- * @since Twenty Twenty-One 1.0
- */
+<!DOCTYPE html>
+<html lang="ru">
 
-?>
-<!doctype html>
-<html <?php language_attributes(); ?> <?php twentytwentyone_the_html_classes(); ?>>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>" />
-	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	<script defer="defer" src="<?php echo bloginfo('template_url'); ?>/main.js"></script>
-	<?php wp_head(); ?>
+    <meta charset="<?php bloginfo( 'charset' ); ?>" />
+    <meta name="viewport" content="width=device-width,initial-scale=1" />
+    <title>Mesto</title>
+    <script defer="defer" src="<?php echo bloginfo('template_url'); ?>/main.js"></script>
+    <link href="<?php echo bloginfo('template_url'); ?>/style.css" rel="stylesheet" />
+    <?php wp_head(); ?>
 </head>
 
 <body class="root">
     <div class="page">
-      <header class="header"><div class="header__logo"></div></header>
-      <main class="content">
+        <header class="header">
+            <div class="header__logo"></div>
+        </header>
+        <main class="content">
--- index.php
+++ index.php
@@ -1,53 +1,20 @@
-<?php
-/**
- * The main template file
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package WordPress
- * @subpackage Twenty_Twenty_One
- * @since Twenty Twenty-One 1.0
- */
-
-get_header(); ?>
-
+<?php get_header(); ?>
 <section class="profile">
-          <div
-            class="profile__avatar"
-            style="background-image: url('3220aa0123cd7dc3fc11.jpg')"
-          >
-            <div class="profile__edit-avatar">
-              <button
-                class="profile__edit-avatar-button"
-                type="button"
-                aria-label="Изменить аватар"
-              ></button>
-            </div>
-          </div>
-          <div class="profile__info">
-            <div class="profile__name-container">
-              <h1 class="profile__name">Жак-Ив Кусто</h1>
-              <button
-                class="profile__edit-button"
-                type="button"
-                aria-label="Редактировать"
-              ></button>
-            </div>
-            <p class="profile__job">Исследователь океана</p>
-          </div>
-          <button
-            class="profile__add-button"
-            type="button"
-            aria-label="Добавить"
-          ></button>
-        </section>
-        <section class="elements" aria-label="Места">
-          <ul class="elements__container"></ul>
-        </section>
-
+    <div class="profile__avatar" style="background-image: url('3220aa0123cd7dc3fc11.jpg')">
+        <div class="profile__edit-avatar">
+            <button class="profile__edit-avatar-button" type="button" aria-label="Изменить аватар"></button>
+        </div>
+    </div>
+    <div class="profile__info">
+        <div class="profile__name-container">
+            <h1 class="profile__name">Жак-Ив Кусто</h1>
+            <button class="profile__edit-button" type="button" aria-label="Редактировать"></button>
+        </div>
+        <p class="profile__job">Исследователь океана</p>
+    </div>
+    <button class="profile__add-button" type="button" aria-label="Добавить"></button>
+</section>
+<section class="elements" aria-label="Места">
+    <ul class="elements__container"></ul>
+</section>
 <?php get_footer(); ?>
